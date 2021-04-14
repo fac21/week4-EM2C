5f78a4116e5ca599ff7ae54632c415aa05b0db98
Mariya Peychinova <mariapeichinova@gmail.com>
2021-04-14 16:49:18 +0100

    rendering the messages on the new page works
    Relates #2

--- server.js
+++ server.js
@@ -1,19 +1,9 @@
 const express = require("express");
+const articles = require('./articles');
 
 const server = express();
 
 server.get("/", (request, response) => {
-
-    const bodyParser = express.urlencoded({ extended: false });
-
-    server.post("/add-dog", bodyParser, (request, response) => {
-        const newMessage = request.body;
-        const message = newMessage.message.toLowerCase();
-        messages[message] = newMessage;
-        response.redirect("/");
-});
-    console.log(message);
-    let items = "";
 
     const html = `
     <!DOCTYPE html>
@@ -34,12 +24,64 @@ server.get("/", (request, response) => {
 
                 <button>Submit</button>
             </form>
+        
+        </body>
+</html>
+    `
+  response.send(html);
+});
+
+server.get('/articles', (request, response) => {
+    
+    let items = "";
+    for (const article of Object.values(articles)) {
+        
+          items += `<li>${article.message} ${article.name}</li>`;
+        
+      }
+
+    const html = `
+    <!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <title>microblog</title>
+        </head>
+        <body>
+            <h1>#</h1>
+            
             <ul>${items}</ul>
         </body>
 </html>
     `
   response.send(html);
 });
+
+
+const bodyParser = express.urlencoded({ extended: false });
+
+server.post("/", bodyParser, (request, response) => {
+    const newMessage = request.body;
+    //const newName = request.body;
+    console.log(newMessage);
+    const message = newMessage.message.toLowerCase();
+    //const name = newMessage.name.toLowerCase();
+    articles[message] = newMessage;
+    //articles[name] = newName;
+    console.log({articles});
+    response.redirect("/articles");
+});
+
+// server.post("/", bodyParser, (request, response) => {
+//     const newMessage = request.body;
+//     console.log({newMessage});
+//     const message2 = newMessage.message.toLowerCase();
+//     //const name2 = newMessage.name.toLowerCase();
+//    // articles[message] = message2;
+//    // articles[name] = name2;
+//     response.redirect("/articles");
+// });
 
 const PORT = 3000;
 
